776d15d0049790ccca2352212daf750691589279
joseph-signed <105678044+joseph-signed@users.noreply.github.com>
2022-08-26 18:03:54 -0500

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web.Mvc;
+using ApplicantProject.Models;
+
+
+namespace ApplicantProject.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            UserModel newUser = new UserModel();
+            return View(newUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                DateTime registerDate;
+                if (!DateTime.TryParse(userModel.RegisterDateString, out registerDate))
+                {
+                    //Handle error
+                }
+                else
+                {
+                    userModel.RegisterDate = registerDate;
+                }
+
+                //Make a Test function that accepts a first name and a last name and returns a string containing the passed in values
+                if (!string.IsNullOrEmpty(userModel.FirstName) && !string.IsNullOrEmpty(userModel.LastName))
+                {
+                    userModel.FullName = userModel.FirstName + " " + userModel.LastName; 
+                }
+                //********************************************************************************************************************
+            }
+
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public JsonResult JsonIndex(UserModel userModel)
+        {
+            string fullName = "";
+
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(userModel.FirstName) && !string.IsNullOrEmpty(userModel.LastName))
+                {
+                    fullName = userModel.FirstName + " " + userModel.LastName;
+                }
+            }
+
+            return Json(new { result = fullName }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ApplicantProject.Models
+{
+    public class UserModel
+    {
+        public UserModel() { }
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public byte Age { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public string RegisterDateString { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
